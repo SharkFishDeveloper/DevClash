b729ed6153ec874763557782363aa7aea4b6fce8
SharkFishDeveloper <shahzebakhtart66@gmail.com>
2026-01-19 19:56:54 +0530

    a

--- util/EditorOnMount.ts
+++ util/EditorOnMount.ts
@@ -1,19 +1,20 @@
-import * as monaco from "monaco-editor";
+import { Monaco } from "@monaco-editor/react";
 
-export default function handleEditorWillMount(monacoInstance: typeof monaco) {
-  // Disable all validation globally
+export default function handleEditorWillMount(monacoInstance: Monaco) {
+  // Disable JavaScript validation
   monacoInstance.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
     noSemanticValidation: true,
     noSyntaxValidation: true,
   });
 
+  // Disable TypeScript validation
   monacoInstance.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
     noSemanticValidation: true,
     noSyntaxValidation: true,
   });
 
   // Optional: disable all markers for any language
-  monacoInstance.editor.onDidCreateModel((model) => {
+  monacoInstance.editor.onDidCreateModel((model: any) => {
     monacoInstance.editor.setModelMarkers(model, 'owner', []);
   });
 }
